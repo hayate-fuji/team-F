91abb1d7b28733630790b566b98d11c6d80e804b
hayate-fuji <hayate.fujita0925@gmail.com>
2021-02-06 18:09:50 +0900

    add images

---
+++ zukan.js
@@ -0,0 +1,211 @@
+<script>
+window.onload = function() {
+
+ var suba = document.getElementById('submita');
+ var subb = document.getElementById('submitb');
+ var subc = document.getElementById('submitc');
+ var subd = document.getElementById('submitd');
+ var sube = document.getElementById('submite');
+ var subf = document.getElementById('submitf');
+ var subg = document.getElementById('submitg');
+ var subh = document.getElementById('submith');
+ var subi = document.getElementById('submiti');
+ var subj = document.getElementById('submitj');
+ var subk = document.getElementById('submitk');
+ var subl = document.getElementById('submitl');
+ var subm = document.getElementById('submitm');
+ var subn = document.getElementById('submitn');
+ var subo = document.getElementById('submito');
+ var subp = document.getElementById('submitp');
+
+ var resulta = document.getElementById('resulta');
+ var resultb = document.getElementById('resultb');
+ var resultc = document.getElementById('resultc');
+ var resultd = document.getElementById('resultd');
+ var resulte = document.getElementById('resulte');
+ var resultf = document.getElementById('resultf');
+ var resultg = document.getElementById('resultg');
+ var resulth = document.getElementById('resulth');
+ var resulti = document.getElementById('resulti');
+ var resultj = document.getElementById('resultj');
+ var resultk = document.getElementById('resultk');
+ var resultl = document.getElementById('resultl');
+ var resultm = document.getElementById('resultm');
+ var resultn = document.getElementById('resultn');
+ var resulto = document.getElementById('resulto');
+ var resultp = document.getElementById('resultp');
+
+ //ボタンを押したときの処理
+
+
+ suba.onclick = function() {
+ var flag = confirm('種\n何かの種');
+ //OKが押されたかキャンセルが押されたかのチェック
+ if(flag) {
+ resulta.innerHTML = "きずぐすりを購入しました。";
+ } else {
+ resulta.innerHTML = "キャンセルされました。";
+ }
+ };
+
+ subb.onclick = function() {
+ var flag = confirm('種\n何かの種');
+ //OKが押されたかキャンセルが押されたかのチェック
+ if(flag) {
+ resultb.innerHTML = "きずぐすりを購入しました。";
+ } else {
+ resultb.innerHTML = "キャンセルされました。";
+ }
+ };
+
+ subc.onclick = function() {
+ var flag = confirm('種\n何かの種');
+ //OKが押されたかキャンセルが押されたかのチェック
+ if(flag) {
+ resultc.innerHTML = "きずぐすりを購入しました。";
+ } else {
+ resultc.innerHTML = "キャンセルされました。";
+ }
+ };
+
+ subd.onclick = function() {
+ var flag = confirm('種\n何かの種');
+ //OKが押されたかキャンセルが押されたかのチェック
+ if(flag) {
+ resultd.innerHTML = "きずぐすりを購入しました。";
+ } else {
+ resultd.innerHTML = "キャンセルされました。";
+ }
+ };
+
+ sube.onclick = function() {
+ var flag = confirm('種\n何かの種');
+ //OKが押されたかキャンセルが押されたかのチェック
+ if(flag) {
+ resulte.innerHTML = "きずぐすりを購入しました。";
+ } else {
+ resulte.innerHTML = "キャンセルされました。";
+ }
+ };
+
+ subf.onclick = function() {
+ var flag = confirm('種\n何かの種');
+ //OKが押されたかキャンセルが押されたかのチェック
+ if(flag) {
+ resultf.innerHTML = "きずぐすりを購入しました。";
+ } else {
+ resultf.innerHTML = "キャンセルされました。";
+ }
+ };
+
+ subg.onclick = function() {
+ var flag = confirm('種\n何かの種');
+ //OKが押されたかキャンセルが押されたかのチェック
+ if(flag) {
+ resultg.innerHTML = "きずぐすりを購入しました。";
+ } else {
+ resultg.innerHTML = "キャンセルされました。";
+ }
+ };
+
+ subh.onclick = function() {
+ var flag = confirm('種\n何かの種');
+ //OKが押されたかキャンセルが押されたかのチェック
+ if(flag) {
+ resulth.innerHTML = "きずぐすりを購入しました。";
+ } else {
+ resulth.innerHTML = "キャンセルされました。";
+ }
+ };
+
+ subi.onclick = function() {
+ var flag = confirm('種\n何かの種');
+ //OKが押されたかキャンセルが押されたかのチェック
+ if(flag) {
+ resulti.innerHTML = "きずぐすりを購入しました。";
+ } else {
+ resulti.innerHTML = "キャンセルされました。";
+ }
+ };
+
+ subj.onclick = function() {
+ var flag = confirm('種\n何かの種');
+ //OKが押されたかキャンセルが押されたかのチェック
+ if(flag) {
+ resultj.innerHTML = "きずぐすりを購入しました。";
+ } else {
+ resultj.innerHTML = "キャンセルされました。";
+ }
+ };
+
+ subk.onclick = function() {
+ var flag = confirm('種\n何かの種');
+ //OKが押されたかキャンセルが押されたかのチェック
+ if(flag) {
+ resultk.innerHTML = "きずぐすりを購入しました。";
+ } else {
+ resultk.innerHTML = "キャンセルされました。";
+ }
+ };
+
+ subl.onclick = function() {
+ var flag = confirm('種\n何かの種');
+ //OKが押されたかキャンセルが押されたかのチェック
+ if(flag) {
+ resultl.innerHTML = "きずぐすりを購入しました。";
+ } else {
+ resultl.innerHTML = "キャンセルされました。";
+ }
+ };
+
+ subm.onclick = function() {
+ var flag = confirm('種\n何かの種');
+ //OKが押されたかキャンセルが押されたかのチェック
+ if(flag) {
+ resultm.innerHTML = "きずぐすりを購入しました。";
+ } else {
+ resultm.innerHTML = "キャンセルされました。";
+ }
+ };
+
+ subn.onclick = function() {
+ var flag = confirm('種\n何かの種');
+ //OKが押されたかキャンセルが押されたかのチェック
+ if(flag) {
+ resultn.innerHTML = "きずぐすりを購入しました。";
+ } else {
+ resultn.innerHTML = "キャンセルされました。";
+ }
+ };
+
+ subo.onclick = function() {
+ var flag = confirm('種\n何かの種');
+ //OKが押されたかキャンセルが押されたかのチェック
+ if(flag) {
+ resulto.innerHTML = "きずぐすりを購入しました。";
+ } else {
+ resulto.innerHTML = "キャンセルされました。";
+ }
+ };
+
+ subp.onclick = function() {
+ var flag = confirm('種\n何かの種');
+ //OKが押されたかキャンセルが押されたかのチェック
+ if(flag) {
+ resultp.innerHTML = "きずぐすりを購入しました。";
+ } else {
+ resultp.innerHTML = "キャンセルされました。";
+ }
+ };
+
+ subq.onclick = function() {
+ var flag = confirm('種\n何かの種');
+ //OKが押されたかキャンセルが押されたかのチェック
+ if(flag) {
+ resultq.innerHTML = "きずぐすりを購入しました。";
+ } else {
+ resultq.innerHTML = "キャンセルされました。";
+ }
+ };
+
+</script>
